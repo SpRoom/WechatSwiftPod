ff225824838a6675d3e30270136f1e69385587cd
spec <spectatornan@gmail.com>
2019-05-22 11:04:15 +0800

    Initial Commit

---
+++ WechatSwiftPod/WechatSwiftPod.h
@@ -0,0 +1,19 @@
+//
+//  WechatSwiftPod.h
+//  WechatSwiftPod
+//
+//  Created by ios-spec on 2019/5/22.
+//  Copyright © 2019 mhly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WechatSwiftPod.
+FOUNDATION_EXPORT double WechatSwiftPodVersionNumber;
+
+//! Project version string for WechatSwiftPod.
+FOUNDATION_EXPORT const unsigned char WechatSwiftPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WechatSwiftPod/PublicHeader.h>
+
+
